fe07b101ba2358fe52ac8845d22676add874fe76
mohammedsbd <mohammed1112ok@gmail.com>
2025-09-28 08:12:17 -0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
       devise_for :users, controllers: {
         sessions: 'users/sessions'
+        registrations: 'users/registrations'
       }
     end
   get "page/home"
