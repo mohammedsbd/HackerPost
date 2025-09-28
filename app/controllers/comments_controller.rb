ed278b4178f609a11f85b16eeec60f8ee3810912
mohammedsbd <mohammed1112ok@gmail.com>
2025-09-28 07:10:25 -0700

    update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+    def comment_params
+        params.require(:comment).permit(:content, :user_id)
+    end
+
+    def create
+        @post = Post.find(params[:post_id])
+
+        @post.comments.create!(comment_params.merge(user: current_user))
+
+        redirect_to @post
+    end
 end
